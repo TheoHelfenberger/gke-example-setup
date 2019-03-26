cad2b392706901ca6c0db72e7de331b796a2776a
Theo Helfenberger <thelfenberger@gmail.com>
2019-03-26 19:52:00 +0100

    add helm chart

--- Jenkinsfile.k8s.groovy
+++ Jenkinsfile.k8s.groovy
@@ -3,43 +3,74 @@ def label = "maven-${UUID.randomUUID().toString()}"
 def service = "microservice"
 def dockerRegistry = "eu.gcr.io"
 def gkeProject = "dd-experiment-registry"
+def imageTag = ""
 
-
-podTemplate(label: label,
-  containers: [
+podTemplate(label: label, imagePullSecrets: ['registry-pull-push'], serviceAccount: 'jenkins',
+containers: [
 	containerTemplate(name: 'maven', image: 'maven:3.3.9-jdk-8-alpine', ttyEnabled: true, command: 'cat'),
-	containerTemplate(name: 'docker', image: 'docker', ttyEnabled: true, command: 'cat')
-  ],
-  volumes: [
+	containerTemplate(name: 'docker', image: 'docker', ttyEnabled: true, command: 'cat'),
+	containerTemplate(name: 'helm-kubectl', image: 'dtzar/helm-kubectl', ttyEnabled: true, command: 'cat')
+],
+volumes: [
 	hostPathVolume(mountPath: '/var/run/docker.sock', hostPath: '/var/run/docker.sock'),
-	configMapVolume(mountPath: '/gcloud', configMapName: 'sagcloudjson')
-  ]) {
+	configMapVolume(mountPath: '/gcloud', configMapName: 'registry-pull-push')
+]) {
 
-  node(label) {
-	container('maven') {
-		stage('Get sources') {
-			checkout scm
+	node(label) {
+
+		container('maven') {
+
+			//			dir('RepoOne') {
+			//				checkout([
+			//					$class: 'GitSCM',
+			//					branches: [[name: 'refs/heads/master']],
+			//					doGenerateSubmoduleConfigurations: false,
+			//					extensions: [
+			//						[$class: 'RelativeTargetDirectory', relativeTargetDir: 'openshift-engineering']
+			//					],
+			//					submoduleCfg: [],
+			//					userRemoteConfigs: [
+			//						[ credentialsId: 'GitHub_login_token2', url: 'https://github.com/e-medicus/openshift-engineering.git']]
+			//				])
+			//			}
+
+			//			dir('RepoOne2') {
+			stage('Get sources') {
+				def scmResult = checkout scm
+				println(scmResult)
+				imageTag = "${scmResult.GIT_COMMIT}".substring(0,6)
+			}
+
+
+			stage('Build a Maven project') {
+				def mvnProps = " -Dfabric8.resourceDir=src/main/fabric8-gke -Dfabric8.generator.name=$gkeProject/it/${service}:${imageTag}"
+				mvn "clean install fabric8:resource fabric8:build -DskipTests=true -Pk8s $mvnProps"
+			}
 		}
-	
-		stage('Build a Maven project') {
-			def mvnProps = " -Dfabric8.resourceDir=src/main/fabric8-gke -Dfabric8.generator.name=$gkeProject/microservice"
-			mvn "clean install fabric8:resource fabric8:build -DskipTests=true -Pk8s $mvnProps"
-			sh "cat target/classes/META-INF/fabric8/kubernetes.yml"
+		//		}
+
+		container('docker') {
+			def tag = "${dockerRegistry}/${gkeProject}/it/${service}:${imageTag}"
+			sh "cat /gcloud/registry-pull-push.json | docker login  -u _json_key --password-stdin https://${dockerRegistry}"
+			sh "docker tag ${gkeProject}/it/${service} ${tag}"
+			sh "docker push $tag"
 		}
-	}
 
-	container('docker') {
-	  def tag = "${dockerRegistry}/${gkeProject}/it/${service}:latest"
-	  sh "cat /gcloud/sa_cluster_kubeplayground.json | docker login  -u _json_key --password-stdin https://${dockerRegistry}"
-	  sh "docker tag ${gkeProject}/it/${service} ${tag}"
-	  sh "docker images"
-	  sh "docker push $tag"
+		container('helm-kubectl') {
+			def tag = "${dockerRegistry}/${gkeProject}/it/${service}:${imageTag}"
+			dir('src/k8s/helm/microservice') {
+				sh """
+					helm upgrade -i --name microservice --values=values.yaml \
+					--namespace it \
+					--set image.repository=${dockerRegistry}/${gkeProject}/it/${service} \
+					--set iamge.tag=${imageTag} \
+					.
+				"""
+			}
 	}
-
-  }
 }
 
 def mvn(String command) {
-   sh "mvn --batch-mode  -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn ${command}"
+	sh "mvn --batch-mode  -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn ${command}"
 }
 
